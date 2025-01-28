43458b2547c1efc5ac3a964ab4b69ef8332dd282
Mercy Babayemi <mercyjanet013@gmail.com>
2025-01-28 09:32:49 +0100

    Update hello.java

--- MercyLala/hello.java
+++ MercyLala/hello.java
@@ -1,9 +1,3 @@
 public class Hello{
-	public static void main(String[] args){
-		System.out.print("Hello Mercy");
-	}
-
-	public void text(){
-		System.out.print("Texting");
-	}
+	first try changing
 }
